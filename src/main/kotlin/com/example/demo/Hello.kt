a03af2e8be0b0c40d10c6f8188d1709e0dc3ddc2
“Sten <sten.uuesoo@gmail.com>
2023-06-07 18:00:59 +0300

    test and controller example added.

--- src/main/kotlin/com/example/demo/Hello.kt
+++ src/main/kotlin/com/example/demo/Hello.kt
@@ -1,16 +1,16 @@
 package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 
 class Hello {
 
-    @GetMapping("/")
+    @GetMapping("/{name}")
 
-    fun Hello() {
-        println("Hello, hello!");
+    fun Hello(@PathVariable name: String): String {
+        return "Hello, hello $name";
     }
-
 }
--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,26 @@
 package com.example.demo
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+	@Autowired
+	private lateinit var mockMvc: MockMvc
 
+	@Test
+	fun `should return greeting for provided name`() {
+		val name = "John"
+		mockMvc.perform(get("/$name"))
+			.andExpect(status().isOk)
+			.andExpect(content().string("Hello, hello $name"))
+	}
 }
